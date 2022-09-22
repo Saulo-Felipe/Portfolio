56815fa4b2fdd6a10a468b698f24e4b900460688
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-09-22 02:44:16 +0000

    physics

--- app/src/components/Stacks/index.tsx
+++ app/src/components/Stacks/index.tsx
@@ -43,43 +43,58 @@ export function Stacks() {
     class MyGame extends Phaser.Scene {
       preload() {
         this.load.image("javascript", "/images/javascript.png");
-				this.load.image("click-here", "/images/click-here.png");
 
         this.load.atlas("languages", "/collisionAssets/sprites.png", "/collisionAssets/sprites.json");
         this.load.json("physics", "/collisionAssets/physics.json");
+
+        this.load.image("sun", "/images/sun2.png");
       }
 
       create() {
         this.matter.world.setBounds();
-				this.add.image(140, 140, "click-here").setRotation(-0.5).alpha = 0.3;
         const spritePhysics = this.cache.json.get("physics");
 
         this.matter.add.mouseSpring({ length: 1, stiffness: 0.6 });
 
-        var teste = this.add.group();
 
-        this.matter.add.image(200, 200, "languages", "javascript", {
+        this.matter.add.image(200, 200, "sun", undefined, {
           shape: spritePhysics.javascript,
           plugin: {
-            attractors: (bodyA, bodyB) => {
-              return {
-                x: (bodyA.position.x - bodyB.position.x) * 0.000001,
-                y: (bodyA.position.y - bodyB.position.y) * 0.000001
-              };
-            }
+            attractors: [
+              (bodyA: any, bodyB: any) => {
+                // bodyB is the sun
+                const x1 = bodyA.position.x
+                const y1 = bodyA.position.y
+
+                const x2 = bodyB.position.x
+                const y2 = bodyB.position.y
+
+                // d=√((x2 – x1)² + (y2 – y1)²).
+
+                const distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+                if (distance > 300) {
+                  return {
+                    x: (bodyA.position.x - bodyB.position.x) * 0.00002,
+                    y: (bodyA.position.y - bodyB.position.y) * 0.00002
+                  };
+                } else {
+                  return {
+                    x: (bodyB.position.x - bodyA.position.x) * 0.00002,
+                    y: (bodyB.position.y - bodyA.position.y) * 0.00002
+                  };
+                }
+              }
+            ]
           }
-        }).setScale(0.3);
+        }).setScale(0.75);
+
 
         this.allLanguages = [];
 
 				this.input.on("pointerdown", (pointer: any) => {
           const language = new Language(spritePhysics, this.matter, pointer)
           this.allLanguages.push(language);
-
-          // language.language.isDead =
-          // teste.add(language.language);
-
-          console.log("group: ", teste);
 				});
       }
 
@@ -97,17 +112,16 @@ export function Stacks() {
       width: window.innerWidth-100,
       height: window.innerHeight-100,
       canvas: canvasRef.current as HTMLCanvasElement,
-			backgroundColor: "#0c101a",
+			backgroundColor: "black",
       physics: {
         default: "matter",
         matter: {
-          plugins: {
-            attractors: true
-          },
           gravity: {
-            scale: 0
-          }
-          // debug: true
+            x: 0,
+            y: 0
+          },
+          "plugins.attractors": true,
+          debug: true
         }
       },
       scene: MyGame
