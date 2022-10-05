fc7b8a7ab3fe0ba348296ca2b5eab1b0dd7aece0
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-10-05 18:47:29 +0000

    stacks/language collision count

--- app/src/components/Stacks/index.tsx
+++ app/src/components/Stacks/index.tsx
@@ -1,11 +1,28 @@
-import { Container, MiniMap } from "./styles";
-import { useEffect, useRef } from "react";
+import { 
+  Container, 
+  MiniMap, 
+  DestroyedLanguages, 
+  Option,
+  Css, Html, Javascript, Node, Postgre, React, ReactNative, Typescript,
+  Title
+} from "./styles";
+import { useEffect, useRef, useState } from "react";
 
 
 export function Stacks() {
   const canvasRef = useRef<HTMLCanvasElement>({} as HTMLCanvasElement);
-  
-  
+  const [destroyedLanguages, setDestroyedLanguages] = useState({
+    javascript: 0,
+    react: 0,
+    node: 0,
+    html: 0,
+    css: 0,
+    postgre: 0,
+    native: 0,
+    typescript: 0
+  });
+  const destroyedLanguagesRef: any = useRef(destroyedLanguages);
+
   async function Init() {
     const Phaser = await import("phaser");
 
@@ -20,14 +37,17 @@ export function Stacks() {
     	private minimap: Phaser.Cameras.Scene2D.Camera;
 
       private allLanguages: Phaser.Physics.Matter.Image[];
-      private allBullets: Phaser.Physics.Matter.Image[];
-      private deadBullets: Phaser.Physics.Matter.Image[];
 
       private particles: Phaser.GameObjects.Particles.ParticleEmitterManager;
 
       private languagePhysics: any;
 
-      sun: Phaser.Physics.Matter.Image;
+      private sun: Phaser.Physics.Matter.Image;
+      private mars: Phaser.Physics.Matter.Image;
+      private moon: Phaser.Physics.Matter.Image;
+      private earth: Phaser.Physics.Matter.Image;
+      private jupiter: Phaser.Physics.Matter.Image;
+
 
       createSpaceShip() {
         this.spaceShip = this.matter.add.image(300, 300, "space-ship", undefined, {
@@ -38,12 +58,15 @@ export function Stacks() {
         this.spaceShip.setFrictionAir(0.05);
         this.spaceShip.setMass(50);
         this.spaceShip.setDepth(3);
+        this.spaceShip.setScale(0.8); 
 
         this.shotDelay = 0;
         this.shotReleased = true;
 
+        this.square = this.add.rectangle(this.spaceShip.x, this.spaceShip.y-this.spaceShip.height/2, 80, 10, 0xff0000)
+        
         this.particles.createEmitter({
-          frame: "red",
+          frame: "blue",
           speed: {
             onEmit: () => {
               return this.spaceShip.body.speed;
@@ -51,7 +74,7 @@ export function Stacks() {
           },
           lifespan: {
             onEmit: () => {
-              return Phaser.Math.Percent(this.spaceShip.body.speed as number, 0, 300) * 2000;
+              return Phaser.Math.Percent(this.spaceShip.body.speed as number, 0, 300) * 6000;
             }
           },
           alpha: {
@@ -65,9 +88,8 @@ export function Stacks() {
       }
 
       isLanguage(label: string) {
-        return ["code", "javascript", "next", "postgre", "css", "html", "node", "react", "native"].indexOf(label) !== -1;
+        return ["typescript", "javascript", "next", "postgre", "css", "html", "node", "react", "native"].indexOf(label) !== -1;
       }
-
 
       createBullet() {
         this.shotReleased = false;
@@ -87,15 +109,49 @@ export function Stacks() {
         }, 400);
       }
 
+      createPlanets() {
+        this.sun = this.matter.add.image(3000/2, 3000/2, "planets", "sun", {
+          label: "sun"
+        }).setCircle(155) as any;
+        this.sun.setStatic(true);
 
+        this.mars = this.matter.add.image(3000/2, 3000/2, "planets", "mars", {
+          label: "mars"
+        }).setCircle(135) as any;
+        this.mars.setMass(500);
+        this.mars.setRotation(-55);
+        this.mars.setDensity(300);
+        this.mars.setBounce(1);
 
+        this.earth = this.matter.add.image(3000/2, 3000/2, "planets", "earth", {
+          label: "earth"
+        }).setCircle(145) as any;
+        this.earth.setMass(500);
+        this.earth.setRotation(-55);
+        this.earth.setDensity(300);
+        this.earth.setBounce(1);
+
+        this.moon = this.matter.add.image(3000/2, 3000/2, "planets", "moon", {
+          label: "moon"
+        }).setCircle(75) as any;
+        this.moon.setMass(500);
+        this.moon.setDensity(300);
+        this.moon.setBounce(1);
+
+        this.jupiter = this.matter.add.image(3000/2, 3000/2, "planets", "jupiter", {
+          label: "jupiter"
+        }).setCircle(155) as any;
+        this.jupiter.setMass(500);
+        this.jupiter.setDensity(300);
+        this.jupiter.setBounce(1);
+      }
 
       changeLanguagePosition(id: number) {
         for (let c = 0; c < this.allLanguages.length; c++) {
           if (this.allLanguages[c].body.id == id) {
             this.allLanguages[c].setPosition(
-              Math.floor(Math.random() * (5000 - 100 + 1) + 100), 
-              Math.floor(Math.random() * (5000 - 100 + 1) + 100)
+              Math.floor(Math.random() * (3000 - 100 + 1) + 100),
+              Math.floor(Math.random() * (3000 - 100 + 1) + 100)
             );
           }
         }
@@ -123,6 +179,13 @@ export function Stacks() {
         }
       }
 
+
+      updateLanguagePoints(label: any) {
+        destroyedLanguagesRef.current[label] += 1;
+        
+        setDestroyedLanguages({ ...destroyedLanguagesRef.current });
+      }
+
       bulletCollidesWithLanguage(language: any) {
         let particle = this.particles.createEmitter({
           frame: "red",
@@ -141,81 +204,72 @@ export function Stacks() {
         particle.onParticleDeath(() => particle.remove(), this);
 
         this.changeLanguagePosition(language.parent.id);
+        this.updateLanguagePoints(language.label);
       }
 
       preload() {
-        this.load.image("background", "/images/background.png");
-        this.load.image("bullet", "/collisionAssets/bullet.png");
-
+        this.load.image("background", "/StacksGameAssets/images/background.png");
+        this.load.image("bullet", "/StacksGameAssets/images/bullet.png");
 
         // Languages
         this.load.atlas(
           "languages",
-          "/languages/languages.png",
-          "/languages/languages.json"
+          "/StacksGameAssets/languages/languages.png",
+          "/StacksGameAssets/languages/languages.json"
         );
-        this.load.json("languagePhysics", "/languages/physics.json");
+        this.load.json("languagePhysics", "/StacksGameAssets/languages/physics.json");
         // ------------------------------------------------------------------------
 
         // Planets
         this.load.atlas(
           "planets",
-          "/planets/sprite.png",
-          "/planets/sprite.json"
+          "/StacksGameAssets/planets/sprite.png",
+          "/StacksGameAssets/planets/sprite.json"
         );
-        this.load.json("planetsPhysics", "/planets/physics.json");
         // ------------------------------------------------------------------------
 
         // spaceship
         this.load.image(
           "space-ship",
-          "/images/space-ship.png"
+          "/StacksGameAssets/space-ship/space-ship.webp"
         );
         this.load.json(
           "space-ship-physics",
-          "/collisionAssets/space-ship.json"
+          "/StacksGameAssets/space-ship/space-ship.json"
         );
         // ------------------------------------------------------------------------
 
         // Particles
         this.load.atlas(
           "space-ship-particle",
-          "/particles/space-ship.png",
-          "/particles/space-ship.json"
-        );        
+          "/StacksGameAssets/particles/space-ship.png",
+          "/StacksGameAssets/particles/space-ship.json"
+        );
       }
 
       create() {
-        this.add.tileSprite(1500, 1500, 5000, 5000, "background");
-        this.matter.world.setBounds(0, 0, 5000, 5000);
+        let background = this.add.tileSprite(3000/2, 3000/2, 3000, 3000, "background");
+        this.matter.world.setBounds(0, 0, 3000, 3000);
+        this.matter.world.disableGravity();
         this.particles = this.add.particles("space-ship-particle").setDepth(2);
         this.languagePhysics = this.cache.json.get("languagePhysics");
-
         this.createSpaceShip();
+        this.createPlanets();
 
         this.cameras.main.startFollow(this.spaceShip).setName("main");
-        this.cameras.main.setBounds(0, 0, 5000, 5000);
+        this.cameras.main.setBounds(0, 0, 3000, 3000);
 
         this.minimap = this.cameras.add(0, 0, 200, 200).setName("mini-map").setZoom(0.1).setBackgroundColor("red");
         this.minimap.startFollow(this.spaceShip);
         this.minimap.inputEnabled = false;
+        this.minimap.ignore(background);
 
         this.allLanguages = [];
-        this.allBullets = [];
-        this.deadBullets = [];
 
-        const planetPhysics = this.cache.json.get("planetsPhysics");
-
-
-        // Collisions
-        function isLanguage(label: string) {
-          return ["code", "javascript", "next", "postgre", "css", "html", "node", "react", "native"].indexOf(label) !== -1;
-        }
 
         this.matter.world.on("collisionstart", (event: any, bodyA: any, bodyB: any) => {
           if (bodyB.label === "bullet") {
-            console.log("bodyb: ", bodyA);
-            if (isLanguage(bodyA.label)) {
+            if (this.isLanguage(bodyA.label)) {
               this.bulletCollidesWithLanguage(bodyA);
             } else if (bodyA.parent.label !== "space-ship") {
               let particle = this.particles.createEmitter({
@@ -231,35 +285,21 @@ export function Stacks() {
                 }
               });
               particle.explode(10, bodyB.position.x, bodyB.position.y);
-      
+
               particle.onParticleDeath(() => particle.remove(), this);
               bodyB.destroy(true);
             }
           }
         });
 
-        this.sun = this.matter.add.image(1500, 1500, "planets", "sun", {
-          shape: planetPhysics.sun,
-          isStatic: true,
-          label: "sun"
-        } as any);
-
-        this.matter.add.image(600, 500, "planets", "mars", {
-          shape: planetPhysics.mars,
-          label: "mars",
-          isStatic: true
-        }).setScale(0.4).setRotation(45);
-
         this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.matter.world.disableGravity();
 
         // Create languages
         for (let c = 0; c < 25; c++) {
           let sortLanguage = Object.keys(this.languagePhysics)[Math.floor(Math.random()*9)];
-          let x = Math.floor(Math.random() * (5000 - 100 + 1) + 100);
-          let y = Math.floor(Math.random() * (5000 - 100 + 1) + 100);
-    
+          let x = Math.floor(Math.random() * (3000 - 100 + 1) + 100);
+          let y = Math.floor(Math.random() * (3000 - 100 + 1) + 100);
+
           let language = this.matter.add.image(x, y, "languages", sortLanguage, {
             shape: this.languagePhysics[sortLanguage],
             label: "language"
@@ -269,18 +309,49 @@ export function Stacks() {
           language.setFrictionAir(0);
           language.setFrictionStatic(0);
           language.setBounce(1);
-          language.setVelocity(Math.floor(Math.random() * (2 - 1 + 1) + 1) == 1 ? 3 : -3)
-          language.thrust(
-            Math.floor(Math.random() * (2 - 1 + 1) + 1) == 1 ? -0.2 : 0.2
-          );
-  
+          // language.setVelocity(Math.floor(Math.random() * (2 - 1 + 1) + 1) == 1 ? 3 : -3)
+          // language.thrust(
+          //   Math.floor(Math.random() * (2 - 1 + 1) + 1) == 1 ? -0.2 : 0.2
+          // );
+
+
           language.setMass(50);
-  
+
           this.allLanguages.push(language);
         }
       }
 
       update() {
+        Phaser.Actions.RotateAroundDistance([this.jupiter], { x: 3000/2, y: 3000/2 }, 0.002, 1200);
+
+        Phaser.Actions.RotateAroundDistance([this.mars], { x: 3000/2, y: 3000/2 }, 0.002, 850);
+        this.mars.setRotation(this.mars.rotation+0.002);
+
+        Phaser.Actions.RotateAroundDistance([this.earth], { x: 3000/2, y: 3000/2 }, 0.002, 650);
+        this.earth.setRotation(this.earth.rotation+0.002);
+
+        Phaser.Actions.RotateAroundDistance([this.moon], { x: this.earth.x, y: this.earth.y }, 0.01, 350);
+        this.moon.setRotation(this.earth.rotation+0.002);
+
+
+        for (let c = 0; c < this.allLanguages.length; c++) {
+          if (this.allLanguages[c].y < this.spaceShip.y) {
+            this.allLanguages[c].y++;
+          } else {
+            this.allLanguages[c].y--;
+          }
+
+          if (this.allLanguages[c].x < this.spaceShip.x) {
+            this.allLanguages[c].x++;
+          } else {
+            this.allLanguages[c].x--;
+          }
+        }
+
+        // this.square.x = this.spaceShip.x;
+        // this.square.y = this.spaceShip.y-this.spaceShip.height/2;
+
+
         // Keys
         this.updateKeysAction();
 
@@ -292,6 +363,7 @@ export function Stacks() {
         }
 
         this.sun.setRotation(this.sun.rotation+=0.005)
+
       }
     }
 
@@ -319,6 +391,10 @@ export function Stacks() {
   useEffect(() => {
     Init();
   }, []);
+  
+  useEffect(() => {
+    destroyedLanguagesRef.current = destroyedLanguages;
+  }, [destroyedLanguages]);
 
   return (
     <Container>
@@ -327,6 +403,73 @@ export function Stacks() {
         ref={canvasRef}
       ></canvas>
       <MiniMap></MiniMap>
+
+      <DestroyedLanguages>
+        <Title>Linguagens destruídas</Title>
+        <Option>
+          <div className={"label-info"}>
+            <Javascript />
+            <div className={"title"}>Javascript</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.javascript}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <React />
+            <div className={"title"}>React</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.react}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <Node />
+            <div className={"title"}>Node</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.node}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <Html />
+            <div className={"title"}>HTML</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.html}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <Css />
+            <div className={"title"}>CSS</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.css}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <ReactNative />
+            <div className={"title"}>React Native</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.native}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <Postgre />
+            <div className={"title"}>PostgreSQL</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.postgre}</div>
+        </Option>
+
+        <Option>
+          <div className={"label-info"}>
+            <Typescript />
+            <div className={"title"}>Typescript</div>
+          </div>
+          <div className={"count"}>{destroyedLanguages.typescript}</div>
+        </Option>        
+      </DestroyedLanguages>
     </Container>
   );
 }
--- app/src/components/Stacks/styles.ts
+++ app/src/components/Stacks/styles.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { SiJavascript, SiHtml5, SiCss3, SiNodedotjs, SiReact, SiPostgresql, SiTypescript } from "react-icons/si";
+import { TbBrandReactNative } from "react-icons/tb";
+
 
 
 export const Container = styled.section`
@@ -30,4 +33,74 @@ export const MiniMap = styled.div`
 
   background-color: rgba(255, 255, 255, 0.05);
   border-radius: 5px;
+`;
+
+export const DestroyedLanguages = styled.div`
+  border: solid 1px rgba(255, 255, 255, 0.4);
+  position: absolute;
+
+  width: 200px;
+
+  top: 15px;
+  right: 15px;
+
+  background-color: rgba(255, 255, 255, 0.07);
+  border-radius: 5px;
+
+  backdrop-filter: blur(15px);
+`;
+
+export const Title = styled.div`
+  font-size: 1.1rem;
+  color: #fff;
+  padding: 0.5rem;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
+export const Option = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+
+  .label-info {
+    display: flex;
+    align-items: center;
+    
+    .title {
+      color: #fff;
+      margin-left: 0.4rem;
+    }
+  }
+
+  .count {
+    color: #fff;
+  }
+`;
+
+
+export const Javascript = styled(SiJavascript)`
+  color: #f7df1d;
+`;
+export const Html = styled(SiHtml5)`
+  color: #e44d26;
+`;
+export const Css = styled(SiCss3)`
+  color: #214ce5;
+`;
+export const React = styled(SiReact)`
+  color: #62dafc;
+`;
+export const ReactNative = styled(TbBrandReactNative)`
+  color: #00a9d9;
+`;
+export const Node = styled(SiNodedotjs)`
+  color: #3c823b;
+`;
+export const Postgre = styled(SiPostgresql)`
+  color: #31648c;
+`;
+export const Typescript = styled(SiTypescript)`
+  color: #2d79c7;
 `;
