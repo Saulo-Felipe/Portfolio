8863db91a067034d26fbbeac199ac747818dd2e5
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-11-08 01:56:53 +0000

    updating home 3D

--- my-portfolio/src/components/Home/index.tsx
+++ my-portfolio/src/components/Home/index.tsx
@@ -2,18 +2,12 @@
 import { useEffect, useRef } from "react";
 import Spline from "@splinetool/react-spline";
 import { IoIosArrowDown } from "react-icons/io";
+import { motion, useInView } from "framer-motion";
 
 import "./styles.scss";
 
 
 export function Home() {
-  let words = [
-    "Saulo Felipe",
-    "Desenvolvedor Web",
-    "Frontend",
-    "Backend",
-  ];
-  const titleRef = useRef<any>(null);
 
   // useEffect(() => {
   //   let currentWordPos = 0;
@@ -43,22 +37,34 @@ export function Home() {
   //   changeWord("create");
   // }, []);
 
+  const sectionRef = useRef<any>(null);
+  const sectionIsInView = useInView(sectionRef);
+
+  useEffect(() => {
+    console.log("View");
+    document.body.scrollTo(0, 0);
+  }, [sectionIsInView]);
+
   return (
-    <section id="home">
+    <motion.section 
+      id="home"
+      ref={sectionRef}
+    >
       {/* <h1 
         className="dinamic-title"
         ref={titleRef}
       ></h1> */}
 
       <Spline 
+        onScroll={() => console.log("Scrolling")}
         id={"home-design"} 
-        scene={"https://prod.spline.design/u-3i6O3KTAH1TlyQ/scene.splinecode"} 
+        scene={"https://draft.spline.design/xIJvmi0Esy07kHWR/scene.splinecode"} 
       />
 
       <div id="go-to-bottom">
         <IoIosArrowDown />
       </div>
         
-    </section>
+    </motion.section>
   );
 }
