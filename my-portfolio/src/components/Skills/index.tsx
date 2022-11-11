cfd63a96f3fabc67db24a82fca2cea5e942a1bd1
Saulo Felipe <77216487+Saulo-Felipe@users.noreply.github.com>
2022-11-11 20:25:45 +0000

    skills title

--- my-portfolio/src/components/Skills/index.tsx
+++ my-portfolio/src/components/Skills/index.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import "./styles.scss";
 
 export function Skills() {
+  const text = "> Habilidades";
+  const titleRef = useRef<any>(null);
+
+  useEffect(() => {
+    let currentPos = 0;
+    let isBack = false;
+
+    function createText(): any {
+      titleRef.current.innerHTML = text.slice(0, currentPos);
+
+      if (isBack) {
+        currentPos--;
+        if (currentPos === 1) isBack = false;
+
+      } else {
+        currentPos++;
+
+        if (currentPos === text.length+1) {
+          isBack = true;
+          return setTimeout(() => createText(), 3000);
+        } 
+      }
+
+      return setTimeout(() => createText(), 100);
+    }
+
+    createText();
+  }, []);
 
   return (
     <section id="skills">
-      <h1>Habilidades</h1>
+      <h1
+        className="skills-title"
+        ref={titleRef}
+      >Habilidades</h1>
     </section>
   );
 }
